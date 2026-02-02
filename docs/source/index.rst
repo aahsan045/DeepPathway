ea827c1cf9d2bab118ede20b473b8c7c5df2c968
aahsan045 <muhammadahtazaz.ahsan@spotlightpathology.com>
2026-02-02 15:28:17 +0000

    adding read the docs conf file

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,7 +1,7 @@
-DeepPathway
+Welcome to DeepPathway's documentation!
 ===========
 
-DeepPathway is a deep learning framework for predicting pathway expression from H&E images.
+DeepPathway is a deep learning framework for predicting pathway expression from H&E images. It uses spatial transcriptomics data to train a model using bimodal contrastive learning approach. Example ST dataset can be acquired from https://huggingface.co/datasets/MahmoodLab/hest.
 
 .. toctree::
    :maxdepth: 2
--- ucell_code.R
+++ ucell_code.R
@@ -2,26 +2,12 @@ library(Matrix)
 library(R.matlab)
 library(UCell)
 library(jsonlite)
-json_data <- fromJSON("/home/e90244aa/Bleep/visium HD processed/pathway_dict_msigdb_hallmark_filtered50.json")
+json_data <- fromJSON("/home/e90244aa/Bleep/GBM_RAVI_etal_2/msig gbm ucell data/pathway_dic_gbm_msig_7.json")
 signatures_list <- lapply(json_data, as.character)
 signatures_list <- as.list(signatures_list)
-names <- c("1",
-           "2",
-           "3",
-           "4",
-           "5",
-           "6",
-           "7",
-           "8",
-           "9",
-           "10",
-           "11",
-           "12",
-           "13",
-           "14",
-           "15")
+names <- c("296_T","334_T","242_T","248_T","259_T")
 for (name in names){
-  link <- paste0("/home/e90244aa/Bleep/visium HD processed/data_",name,"_part_56.mat")
+  link <- paste0("/home/e90244aa/Bleep/GBM_RAVI_etal_2/msig gbm ucell data/",name,"_data.mat")
   sample <- readMat(link)
   data <- sample$x
   cell_names <- unlist(sample$cell.names)
@@ -31,7 +17,7 @@ for (name in names){
   colnames(DgCMatrix_obj) <- cell_names
   gene_names <- as.vector(trimws(unlist(sample$gene.names)))
   rownames(DgCMatrix_obj) <- gene_names
-  scores <- ScoreSignatures_UCell(DgCMatrix_obj, features=signatures_list,maxRank=645)
-  link <- paste0("/home/e90244aa/Bleep/visium HD processed/",name,"_pathway expression.csv")
+  scores <- ScoreSignatures_UCell(DgCMatrix_obj, features=signatures_list,maxRank=2106)
+  link <- paste0("/home/e90244aa/Bleep/GBM_RAVI_etal_2/msig gbm ucell data/",name,"_pathway expression_2.csv")
   write.csv(scores, file = link, row.names = TRUE)
 }

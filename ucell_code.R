41c87fa8759bd33ab0d90d40fd5ed50745e09067
aahsan045 <muhammadahtazaz.ahsan@spotlightpathology.com>
2026-02-21 12:08:56 +0000

    adding training.ipynb back to github.

--- ucell_code.R
+++ ucell_code.R
@@ -2,12 +2,12 @@ library(Matrix)
 library(R.matlab)
 library(UCell)
 library(jsonlite)
-json_data <- fromJSON("/home/e90244aa/Bleep/GBM_RAVI_etal_2/msig gbm ucell data/pathway_dic_gbm_msig_7.json")
+json_data <- fromJSON("/home/e90244aa/visium HD/Esophagus data/pathway_dict_progeny_filtered.json")
 signatures_list <- lapply(json_data, as.character)
 signatures_list <- as.list(signatures_list)
-names <- c("296_T","334_T","242_T","248_T","259_T")
+names <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15")
 for (name in names){
-  link <- paste0("/home/e90244aa/Bleep/GBM_RAVI_etal_2/msig gbm ucell data/",name,"_data.mat")
+  link <- paste0("/home/e90244aa/visium HD/Esophagus data/",name,"_part_56.mat")
   sample <- readMat(link)
   data <- sample$x
   cell_names <- unlist(sample$cell.names)
@@ -17,7 +17,7 @@ for (name in names){
   colnames(DgCMatrix_obj) <- cell_names
   gene_names <- as.vector(trimws(unlist(sample$gene.names)))
   rownames(DgCMatrix_obj) <- gene_names
-  scores <- ScoreSignatures_UCell(DgCMatrix_obj, features=signatures_list,maxRank=2106)
-  link <- paste0("/home/e90244aa/Bleep/GBM_RAVI_etal_2/msig gbm ucell data/",name,"_pathway expression_2.csv")
+  scores <- ScoreSignatures_UCell(DgCMatrix_obj, features=signatures_list,maxRank=205)
+  link <- paste0("/home/e90244aa/visium HD/Esophagus data/",name,"_pathway expression.csv")
   write.csv(scores, file = link, row.names = TRUE)
 }

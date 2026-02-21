b6416ffa913b1b85e185279fc1b00b6ac17da61e
aahsan045 <muhammadahtazaz.ahsan@spotlightpathology.com>
2026-02-21 12:12:09 +0000

    updated ucell.R file

--- ucell_code.R
+++ ucell_code.R
@@ -2,12 +2,12 @@ library(Matrix)
 library(R.matlab)
 library(UCell)
 library(jsonlite)
-json_data <- fromJSON("/home/e90244aa/visium HD/Esophagus data/pathway_dict_progeny_filtered.json")
+json_data <- fromJSON("/home/e90244aa/Bleep/DeepPathwayV2/prostate cancer dataset/pathway_dict_20.json")
 signatures_list <- lapply(json_data, as.character)
 signatures_list <- as.list(signatures_list)
-names <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15")
+names <- c("MEND61","MEND62")
 for (name in names){
-  link <- paste0("/home/e90244aa/visium HD/Esophagus data/",name,"_part_56.mat")
+  link <- paste0("/home/e90244aa/Bleep/DeepPathwayV2/prostate cancer dataset/data for Ucell calculations/",name,"_data.mat")
   sample <- readMat(link)
   data <- sample$x
   cell_names <- unlist(sample$cell.names)
@@ -17,7 +17,7 @@ for (name in names){
   colnames(DgCMatrix_obj) <- cell_names
   gene_names <- as.vector(trimws(unlist(sample$gene.names)))
   rownames(DgCMatrix_obj) <- gene_names
-  scores <- ScoreSignatures_UCell(DgCMatrix_obj, features=signatures_list,maxRank=205)
-  link <- paste0("/home/e90244aa/visium HD/Esophagus data/",name,"_pathway expression.csv")
+  scores <- ScoreSignatures_UCell(DgCMatrix_obj, features=signatures_list,maxRank=1543)
+  link <- paste0("/home/e90244aa/Bleep/DeepPathwayV2/prostate cancer dataset/pathway expression/",name,"_pathway expression.csv")
   write.csv(scores, file = link, row.names = TRUE)
 }
